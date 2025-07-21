96d0f0746f14b749aa5b6cc3bcf650f2c6942212
5imyboy <simonyong05@gmail.com>
2025-07-21 12:03:03 -0500

    initialize frontend with react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Workout Tracker Fitstack</h1>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
