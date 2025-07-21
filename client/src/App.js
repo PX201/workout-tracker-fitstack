75b4880aeaf28939bef02ff86deef6f487ec84ed
5imyboy <simonyong05@gmail.com>
2025-07-21 15:08:27 -0500

    initialize body highlighter page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
+import BodyHighlighter from "./BodyHighlighter";
 
 function App() {
   return (
     <BrowserRouter>
       <h1>Workout Tracker Fitstack</h1>
       <Routes>
+        <Route path="/body" element={<BodyHighlighter />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/BodyHighlighter.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import Model from 'react-body-highlighter';
+
+function BodyHighlighter() {
+  const [body, setBody] = useState([]);
+
+  // TEST DATA
+  useEffect(() => {
+    // TODO: replace with fetch() call
+    setBody([
+      { name: 'Bench Press', muscles: ['chest', 'triceps', 'front-deltoids'] },
+      { name: 'Push Ups', muscles: ['chest'] },
+    ]);
+    console.log(body);
+  }, []);
+
+  // TODO: replace click with hover
+  const handleClick = React.useCallback(({ muscle, data }) => {
+    const { exercises, frequency } = data;
+    alert(`You clicked the ${muscle}! You've worked out this muscle ${frequency} times through the following exercises: ${JSON.stringify(exercises)}`)
+  }, [body]);
+
+  return (
+    <>
+      <section className="container">
+        <h2 className="text-center">Muscle Groups Worked On</h2>
+        <div className="row">
+          <div className="col-2"></div>
+          <Model className="col-4"
+            data={body}
+            style={{margin: "auto", width: '25rem', padding: '5rem' }}
+            onClick={handleClick}
+          />
+          <Model className="col-4"
+            data={body}
+            style={{margin: "auto", width: '25rem', padding: '5rem' }}
+            onClick={handleClick}
+            type="posterior"
+          />
+          <div className="col-2"></div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default BodyHighlighter;
