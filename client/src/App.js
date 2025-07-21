980db14a762c56244b3d5d3d3adf070002e563eb
5imyboy <simonyong05@gmail.com>
2025-07-21 17:20:24 -0500

    skeleton registration screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Login from "./Login";
+import Registration from "./Registration";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>}></Route>
+        <Route path="/registration" element={<Registration/>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/Registration.jsx
+++ client/src/Registration.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
-function Login() {
+function Registration() {
 
   const handleSubmit = () => {
-    // TODO: handle login
+    // TODO: handle registration, add new user
   }
 
   return (
@@ -11,11 +11,15 @@ function Login() {
       <section className="container-sm mt-5">
         <div className="text-center mb-4">
           <h1 className="mb-4">Fitstack Workout Tracker</h1>
-          <h2>Login:</h2>
+          <h2>Register:</h2>
         </div>
         <div className="row">
           <div className="col-4"></div>
           <form onSubmit={handleSubmit} className="col-4 border border-muted rounded p-4">
+            <fieldset className="mb-4">
+              <label htmlFor="username">Username</label>
+              <input type="text" className="form-control" id="username" />
+            </fieldset>
             <fieldset className="mb-4">
               <label htmlFor="email">Email address</label>
               <input type="email" className="form-control" id="email" />
@@ -24,20 +28,24 @@ function Login() {
               <label htmlFor="password">Password</label>
               <input type="password" className="form-control" id="password" />
             </fieldset>
+            <fieldset className="mb-4">
+              <label htmlFor="password">Repeat password</label>
+              <input type="password" className="form-control" id="password" />
+            </fieldset>
             <div className="d-flex justify-content-between">
               <Link
                 type="button"
                 className="btn btn-dark"
                 to={"/"}
               >
-                Login
+                Register
               </Link>
               <Link
                 type="button"
                 className="btn btn-link d-flex"
-                to={"/registration"}
+                to={"/"}
               >
-                New User?
+                Returning User?
               </Link>
             </div>
           </form>
@@ -48,4 +56,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Registration;
