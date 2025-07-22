50cf240e2e42e0b477106993dff08204e1c8fd7e
PX201 <lahmamsiaiman99@gmail.com>
2025-07-21 22:08:03 -0700

    Add validation to UserService methods and Create custom exception

---
+++ server/workout-tracker/src/main/java/com/fitstack/workout_tracker/dto/UserUpdateRequest.java
@@ -0,0 +1,24 @@
+package com.fitstack.workout_tracker.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserUpdateRequest {
+
+    @Min(value = 1, message = "required")
+    private long userId;
+
+    @NotBlank(message = " is required")
+    @Size(min = 5, max = 50, message = " must be between 5 and 50 characters")
+    private String username;
+
+    @NotBlank(message = " is required")
+    @Email(message = " must be valid")
+    private String email;
+
+
+}
