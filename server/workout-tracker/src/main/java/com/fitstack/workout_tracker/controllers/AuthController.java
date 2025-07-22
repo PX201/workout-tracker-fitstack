71448a9fe3c414f06890157102063de8a05021a2
PX201 <lahmamsiaiman99@gmail.com>
2025-07-21 19:06:50 -0700

    Update UserController.register to handle validation errors

--- server/workout-tracker/src/main/java/com/fitstack/workout_tracker/controllers/AuthController.java
+++ server/workout-tracker/src/main/java/com/fitstack/workout_tracker/controllers/AuthController.java
@@ -1,10 +1,13 @@
 package com.fitstack.workout_tracker.controllers;
 
+import com.fitstack.workout_tracker.domain.Result;
 import com.fitstack.workout_tracker.domain.UserService;
 import com.fitstack.workout_tracker.dto.RegisterRequest;
 import com.fitstack.workout_tracker.models.User;
+import com.fitstack.workout_tracker.utils.ErrorResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/auth")
 public class AuthController {
 
+    @Autowired
     UserService userService;
 
-    @PostMapping("register")
-    public ResponseEntity<User> register(@Valid @RequestBody RegisterRequest request){
-        User user = userService.register(request);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request){
+        Result<User> result = userService.register(request);
+        if (!result.isSuccess()) {
+            return ErrorResponse.build(result);
+        }
+        return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
     }
 
 
--- server/workout-tracker/src/main/java/com/fitstack/workout_tracker/models/User.java
+++ server/workout-tracker/src/main/java/com/fitstack/workout_tracker/models/User.java
@@ -1,9 +1,5 @@
 package com.fitstack.workout_tracker.models;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
