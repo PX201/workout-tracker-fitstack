3b89939b126d66dec355423e73f52bd5ce81e93e
5imyboy <simonyong05@gmail.com>
2025-07-23 01:48:40 -0500

    display and activate users

--- client/src/AdminNavbar.jsx
+++ client/src/AdminNavbar.jsx
@@ -1,9 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { DEFAULT_USER, userUrl } from "./components/UserInfo.js" 
 
 function AdminNavbar() {
-  const [user, setUser] = useState("<username>");
+  const [user, setUser] = useState(DEFAULT_USER);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // fetch admin info
+    const init = { 
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${sessionStorage.getItem("me")}`
+      }
+    };
+    fetch(userUrl, init)
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      }
+    }).then(data => {
+      if (data) {
+        setUser(data);
+      }
+    });
+  }, []);
 
   const handleLogout = () => {
     if (window.confirm("Are you sure you want to log out?")) {
@@ -13,7 +34,7 @@ function AdminNavbar() {
 
   return (
     <nav className="navbar navbar-expand-lg bg-light">
-      <h2 className="me-auto ps-4">Welcome {user}</h2>
+      <h2 className="me-auto ps-4">Welcome {user.username}</h2>
       <div className="navbar-nav ms-auto p-2 pe-4">
           <Link type="button" className="active nav-link ms-4" to={'/admin/users'}>User List</Link>
           <Link type="button" className="active nav-link ms-4" to={'/admin/routines'}>Routine List</Link>
--- client/src/UserNavbar.jsx
+++ client/src/UserNavbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { DEFAULT_USER, userUrl } from "./components/userInfo.js" 
+import { DEFAULT_USER, userUrl } from "./components/UserInfo.js" 
 
 function UserNavbar() {
   const [user, setUser] = useState(DEFAULT_USER);
