ea784effe0171fabc69e96c9cce3817770e74e32
Doug White <whitedoug21@gmail.com>
2025-07-23 22:25:08 -0400

    err

--- client/src/EditLogForm.jsx
+++ client/src/EditLogForm.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate, Link } from "react-router-dom";
-import UserNavbar from "./UserNavbar";
 
 function EditLog() {
   const location = useLocation();
