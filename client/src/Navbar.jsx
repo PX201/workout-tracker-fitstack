76c34cd22981a717838bbf504f601d65b1f948f2
PX201 <lahmamsiaiman99@gmail.com>
2025-07-24 13:14:51 -0700

    Style NotFound and Home, Fix wrong link redirection in the navbar

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -29,8 +29,8 @@ const DefaultNavbar = () => {
 
         <div className="collapse navbar-collapse" id="defaultNavbar">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/">Login</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/registration">Register</Link></li>
           </ul>
         </div>
       </div>
